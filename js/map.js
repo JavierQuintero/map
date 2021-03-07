55053663df88a5926291035d70ea4e5996ae153c
Javier <ejviquin@gmail.com>
2021-03-07 14:41:00 -0500

    funcion de agregar marcadores con doble click y se accede a la ubicacion del ususario

--- js/map.js
+++ js/map.js
@@ -6,12 +6,40 @@ L.tileLayer(tilesProvider,{
     maxZoom: 18
 }).addTo(myMap)
 
+let markerDefauld = L.marker([4.59524, -74.07037]).addTo(myMap)
+
 let iconMarker = L.icon({
 
     iconUrl: './icon/flutter.svg',
     iconSize: [40, 40],
     iconAnchor: [20, 40]
-    
+
 })
 
-L.marker([4.59524, -74.06937], { icon: iconMarker}).addTo(myMap)
+let markerFlutter = L.marker([4.59524, -74.06937], { icon: iconMarker}).addTo(myMap)
+
+myMap.doubleClickZoom.disable()
+myMap.on('dblclick', e => {
+  let latLng = myMap.mouseEventToLatLng(e.originalEvent);
+
+  L.marker([latLng.lat, latLng.lng], { icon: iconMarker }).addTo(myMap)
+})
+
+navigator.geolocation.getCurrentPosition(
+    (pos) => {
+      const { coords } = pos
+      const { latitude, longitude } = coords
+      L.marker([latitude, longitude], { icon: iconMarker }).addTo(myMap)
+  
+      setTimeout(() => {
+        myMap.panTo(new L.LatLng(latitude, longitude))
+      }, 5000)
+    },
+    (error) => {
+      console.log(error)
+    },
+    {
+      enableHighAccuracy: true,
+      timeout: 5000,
+      maximumAge: 0
+    })
